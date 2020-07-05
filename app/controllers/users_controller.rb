d6ab56291e5d13b44f6835885892503f4a73906e
ymrt43 <ymrt43@gmail.com>
2020-07-05 16:08:17 +0900

    Put update method in users_controller.rb

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,11 @@ class UsersController < ApplicationController
   end
 
   def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
+
 end
