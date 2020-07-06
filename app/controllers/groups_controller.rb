193763aa4e9738ecf78b8ef679055c667a1a8f4e
ymrt43 <ymrt43@gmail.com>
2020-07-06 21:28:01 +0900

    Update which view to return when groups#create

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -5,6 +5,12 @@ class GroupsController < ApplicationController
   end
 
   def create
-    binding.pry
+    @group = Group.new(group_params)
+    if @group.save
+      redirect_to root_path, notice: 'グループを作成しました'
+    else
+      render :new
+    end
   end
 end
+
