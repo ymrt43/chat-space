e09f314243e6e98718cf3326dca5515baa73e98e
ymrt43 <ymrt43@gmail.com>
2020-07-08 11:35:54 +0900

    Add before_action in messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,7 +1,15 @@
 class MessagesController < ApplicationController
+  before_action :set_group
+  
   def index
   end
 
   def create
+  end
+
+  private
+
+  def set_group
+    @group = Group.find(params[:group_id])
   end
 end
