a03fba37218e448148265da9d076cb1a9e46212c
ymrt43 <ymrt43@gmail.com>
2020-07-08 11:37:34 +0900

    Update #index in messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,8 @@ class MessagesController < ApplicationController
   before_action :set_group
   
   def index
+    @message = Message.new
+    @messages = @group.messages.includes(:user)
   end
 
   def create
