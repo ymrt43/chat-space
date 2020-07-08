3d8eed7d81366cad97639ecdf00a88b7a93728af
ymrt43 <ymrt43@gmail.com>
2020-07-08 11:41:20 +0900

    Add definition of message_params in messages_controller.rb

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -7,9 +7,13 @@ class MessagesController < ApplicationController
   end
 
   def create
+    @message = Message.new(message_params)
   end
 
   private
+
+  def message_params
+    params.require(:message).permit(:content, :image).merge(user_id: current_user.id)
 
   def set_group
     @group = Group.find(params[:group_id])
