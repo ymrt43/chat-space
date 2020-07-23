3a5dd747c030e4b1e2644cf88ed0c642eb791d99
ymrt43 <ymrt43@gmail.com>
2020-07-23 23:27:12 +0900

    Add html when incremental search in users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,18 +1,45 @@
-$(function(){
+$(function() {
+  function addUser(user) {
+    let html = `
+                <div class="ChatMember clearfix">
+                  <p class="ChatMember__name">${user.name}</p>
+                  <div class="ChatMember__add ChatMember__button" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
+                </div>
+                `;
+    $("#UserSearchResult").append(html);
+  }
 
-  $("#UserSearch__field").on('keyup', function(){
+  function addNoUser() {
+    let html = `
+                <div class="ChatMember clearfix">
+                  <p class="ChatMember__name">ユーザーが見つかりません</p>
+                </div>
+                `;
+    $("#UserSearchResult").append(html);
+  }
+
+  $("#UserSearch__field").on("keyup", function() {
     let input = $("#UserSearch__field").val();
     $.ajax({
-      type: 'GET',
-      url: '/users',
+      type: "GET",
+      url: "/users",
       data: { keyword: input },
-      dataType: 'json'
+      dataType: "json"
     })
-    .done(function(users){
-      console.log("成功");
+    .done(function(users) {
+      $("#UserSearchResult").empty();
+      if (users.length !== 0) {
+        users.forEach(function(user) {
+          addUser(user);
+        });
+      } else if (input.length == 0) {
+        return false;
+      } else {
+        addNoUser();
+      }
     })
-    .fail(function(){
-      console.log("失敗");
+    .fail(function() {
+      alert("通信エラーです。ユーザーが表示できません。");
     });
   });
 });
