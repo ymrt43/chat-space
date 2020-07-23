d4652d3cf7f4a925a86e8df11260caeee4476447
ymrt43 <ymrt43@gmail.com>
2020-07-24 00:55:30 +0900

    Add function when click to add&remove member in users.js

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -18,6 +18,17 @@ $(function() {
     $("#UserSearchResult").append(html);
   }
 
+  function addMember(name, id) {
+    let html = `
+                <div class="ChatMember">
+                  <p class="ChatMember__name">${name}</p>
+                  <input name="group[user_ids][]" type="hidden" value="${id}" />
+                  <div class="ChatMember__remove ChatMember__button">削除</div>
+                </div>
+                `;
+    $(".ChatMembers").append(html);
+  }
+
   $("#UserSearch__field").on("keyup", function() {
     let input = $("#UserSearch__field").val();
     $.ajax({
@@ -41,5 +52,14 @@ $(function() {
     .fail(function() {
       alert("通信エラーです。ユーザーが表示できません。");
     });
+  });
+  $("#UserSearchResult").on("click", ".ChatMember__add", function() {
+    const userName = $(this).attr("data-user-name");
+    const userId = $(this).attr("data-user-id");
+    $(this).parent().remove();
+    addMember(userName, userId);
+  });
+  $(".ChatMembers").on("click", ".ChatMember__remove", function() {
+    $(this).parent().remove();
   });
 });
