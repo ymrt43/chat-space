cde4b0a28f0c3593095e5f2a147954d33f984d50
ymrt43 <ymrt43@gmail.com>
2020-07-23 13:56:19 +0900

    Add json HTML in message.js

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,45 @@
 $(function(){
+  function buildHTML(message){
+    if ( message.image ) {
+      let html =
+        `<div class="Chat">
+          <div class="Chat__info">
+            <div class="Chat__info--name">
+              ${message.user.name}
+            </div>
+            <div class="Chat__info--dateTime">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Chat__message">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+            <img class="Message__image" src="${message.image}">
+          </div>
+        </div>`
+        return html;
+      } else {
+        let html =
+        `<div class="Chat">
+          <div class="Chat__info">
+            <div class="Chat__info--name">
+              ${message.user_name}
+            </div>
+            <div class="Chat__info--dateTime">
+              ${message.created_at}
+            </div>
+          </div>
+          <div class="Chat__message">
+            <p class="Message__content">
+              ${message.content}
+            </p>
+          </div>
+        </div>`
+        return html;
+    };
+  }
+  
   $('.Form').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -10,6 +51,12 @@ $(function(){
       dataType: 'json',
       processData: false,
       contentType: false
+    })
+    .done(function(data){
+      let html = buildHTML(data);
+      // $('.Chat').append(html);
+      // $('.Form__inputContent--textfield').val('');
+      // $('.Form__submit').prop('disabled', false);
     })
   })
 });
